63eeb6915ac2019cc13e52f7d37198853b637b66
Vasco <vasco.vjca@gmail.com>
2023-01-26 12:34:49 +0100

    Incluye la estructura, los dos atributos y el constructor de la segunda clase

---
+++ Procesador.java
@@ -0,0 +1,11 @@
+public class Procesador
+{
+    private double velocidad;
+    private boolean graficosIntegrados;
+
+    public Procesador()
+    {
+        velocidad = 0;
+        graficosIntegrados = true;
+    }
+}
