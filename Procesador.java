5048916e1a24569bd7bf0e01a6075c4601662eb5
JesusRiestra <jriestraricote@gmail.com>
2023-01-31 12:10:57 +0100

    Incluye los metodos getter y setter para cada uno de los dos atributos de la segunda clase

--- Procesador.java
+++ Procesador.java
@@ -8,4 +8,22 @@ public class Procesador
         velocidad = 0;
         graficosIntegrados = true;
     }
+    
+    public double setVelocidad(double velocidad) {
+       this.velocidad = velocidad;
+       return this.velocidad;
+    }
+    
+    public boolean setGraficosIntegrados(boolean graficosIntegrados) {
+        this.graficosIntegrados = graficosIntegrados;
+        return this.graficosIntegrados;
+    }
+    
+    public double getVelocidad() {
+        return velocidad;
+    }
+    
+    public boolean getTieneGraficosIntegrados() {
+        return graficosIntegrados;
+    }
 }
