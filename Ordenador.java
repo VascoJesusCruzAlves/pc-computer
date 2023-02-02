525d1640484779d1ef9bddbce038d38a6ace78a3
JesusRiestra <jriestraricote@gmail.com>
2023-02-02 11:11:39 +0100

    Incluye los métodos getter para cada uno de los tres atributos de la primera clase

--- Ordenador.java
+++ Ordenador.java
@@ -1,12 +1,25 @@
 public class Ordenador {
-    
+
     private String marca;
     private long numeroDeSerie;
     private Procesador procesador;
-    
+
     public Ordenador(String marca, long numeroDeSerie, Procesador procesador) {
-        marca = marca;
-        numeroDeSerie = numeroDeSerie;
-        procesador = procesador;
+        this.marca = marca;
+        this.numeroDeSerie = numeroDeSerie;
+        this.procesador = procesador;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public long getNumeroDeSerie() {
+        return numeroDeSerie;
+    }
+    
+    public Procesador getProcesador() {
+        return procesador;
     }
 }
+
